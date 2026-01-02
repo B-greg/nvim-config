363014cc754aa8446c9346847f4044df80dad77c
gregoire barret <gregoire.barret@accenture.com>
2026-01-02 10:15:45 +0700

    update

--- lua/plugins/better-escape.lua
+++ lua/plugins/better-escape.lua
@@ -3,7 +3,8 @@ return {
     "max397574/better-escape.nvim",
     config = function()
       require("better_escape").setup {
-        default_mappings = true,
+        timeout = vim.o.timeoutlen,
+        default_mappings = false,
         mappings = {
           i = {
             j = {
