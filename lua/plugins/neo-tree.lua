ccb44142287081ec03d17edb70570779e1731edc
gregoire barret <gregoire.barret@accenture.com>
2026-01-13 16:38:23 +0700

    feat: neo-tree size to 17%

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -4,7 +4,7 @@ return {
     opts = {
       window = {
         position = "left",
-        width = "20%", -- Adjust this number as needed
+        width = "17%", -- Adjust this number as needed
       },
     },
   },
