a24d361cf488cdd0e78424cfd2864a197a8f672a
gregoire barret <gregoire.barret@accenture.com>
2026-01-13 16:28:37 +0700

    feat: setup neo-tree size

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      window = {
+        position = "left",
+        width = "20%", -- Adjust this number as needed
+      },
+    },
+  },
+}
