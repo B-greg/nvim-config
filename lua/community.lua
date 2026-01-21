02e62b6ec9f77d0d394912f42dbf72ff205a1afe
greg <barret.gregoire@gmail.com>
2026-01-21 23:00:51 +0700

    add rust

--- lua/community.lua
+++ lua/community.lua
@@ -26,6 +26,7 @@ return {
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.helm" },
+  { import = "astrocommunity.pack.rust" },
   -- { import = "astrocommunity.recipes.ai" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.search.nvim-spectre" },
