9dd777b128e7d884614da8863af2534d86969a69
gregoire barret <gregoire.barret@accenture.com>
2026-02-10 18:16:07 +0700

    feat: add icons

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -83,7 +83,7 @@ return {
 
         -- tables with the `name` key will be registered with which-key if it's installed
         -- this is useful for naming menus
-        ["<leader>b"] = { name = "Buffers" },
+        ["<leader>b"] = { name = "Buffers", desc = utils.get_icon("", "", 1) .. "Test" },
         -- quick save
         -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" }, -- change description but the same command
 
