9da9421aa3515bc2cede652acf12a79b9f7917ce
gregoire barret <gregoire.barret@accenture.com>
2026-06-05 16:05:20 +0700

    add markdown viewer

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -45,7 +45,8 @@ return {
         number = true, -- sets vim.opt.number
         spell = false, -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
-        wrap = false, -- sets vim.opt.wrap
+        wrap = true, -- sets vim.opt.wrap
+        colorcolumn = "80,120", -- sets vim.opt.colorcolumn
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
